e4c036b1626435f2913582491a76f1dacb12ea82
Andrew Brudnak <brudnak@icloud.com>
2019-05-05 19:00:53 -0700

    Completed arrays.js challenge #2
    - Also removed unused export from arrays.js
    
     Test Suites: 3 passed, 3 total
      Tests:       11 passed, 11 total
      Snapshots:   0 total
      Time:        2.025s, estimated 3s
      Ran all test suites.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -79,8 +79,10 @@ console.log(searchID(42))
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+// let lastCar = 0;
+let lastCar = inventory[inventory.length - 1]
+const { car_make, car_model } = lastCar
+console.log(`The last vehicle in inventory is a ${car_make} ${car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -104,7 +106,6 @@ console.log();
 
 // Exports
 module.exports = {
-    searchID,
-    inventory
+    searchID
   }
-
+ 
